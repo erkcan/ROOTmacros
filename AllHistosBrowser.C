9632f81f9da00f2f6f97c24cf40eb3f5ecfafbea
erkcan <erkcan>
2006-09-26 16:39:01 +0000

    * An option string can be passed to Draw() * Better delays - accept actual number of seconds

--- AllHistosBrowser.C
+++ AllHistosBrowser.C
@@ -1,6 +1,6 @@
 // A ROOT macro to plot all the open histograms one by one.
 
-int AllHistosBrowser(long delay=2e6)
+int AllHistosBrowser(float delay=1.5, TString opt="")
 {
   int noOfPlottedHistos = 0;
   int noOfBrowsables = gDirectory->GetListOfKeys()->GetEntries();
@@ -9,11 +9,11 @@ int AllHistosBrowser(long delay=2e6)
   for (int k=0; k<noOfBrowsables; k++) {
     TObject *myobj = gROOT->FindObject(gDirectory->GetListOfKeys()->At(k)->GetName());
     if ( myobj != 0 && myobj->InheritsFrom("TH1") ) {
-      myobj->Draw();
+      myobj->Draw(opt.Data());
       mycanvas->Update();
       cout << myobj->GetName() << endl;
       noOfPlottedHistos++;
-      for (long w=0; w<delay; w++) continue;
+      TStopwatch wt; while (wt.RealTime()<delay) wt.Continue();
     }
   }
   return noOfPlottedHistos;
