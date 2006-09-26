7bdf7f2bdc99fa31aea79a8b176dc2b760499252
erkcan <erkcan>
2006-09-26 14:00:49 +0000

    * Fix major bug in filling the TTree * Remove the unnecessary creation of the TFile * Add possibility of giving the histogram x-range

--- PlotASCII.C
+++ PlotASCII.C
@@ -1,11 +1,15 @@
 // A ROOT macro that reads in numbers from a given ASCII file,
-// fills them into a TTree.
+// fills them into a TTree and plots them on a histogram.
 
-int PlotASCII(TString filename)
+// Just fill the TTree and use ROOT's own TTree->Draw()
+int PlotASCII(TString filename) { return PlotASCII(filename,0,0); }
+
+// xmin and xmax are the limits of the histogram to be created.
+// Any value read outside [xmin,xmax] range will become under/overflow.
+int PlotASCII(TString filename, float xmin, float xmax)
 {
-  TFile *myfile = new TFile("plotascii.root","RECREATE",filename+" ROOT file");
   TTree *mytree = new TTree(filename,filename+" ROOT tree");
-  double x;
+  float x;
   mytree->Branch(filename,&x,filename);
 
   ifstream asciiFile(filename);
@@ -17,6 +21,10 @@ int PlotASCII(TString filename)
       sayac++;
     }
 
-  mytree->Draw();
+  TH1F *myhisto;
+  if (xmin!=xmax) myhisto = new TH1F(filename+"_histo",filename+"_histo", 100, xmin, xmax);
+  else myhisto = new TH1F();
+  mytree->Draw(filename+">>"+filename+"_histo");
+
   return sayac;
 }
