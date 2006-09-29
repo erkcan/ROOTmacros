103b30a9a18774b252a9c94b988808fc67834682
erkcan <erkcan>
2006-09-29 15:15:34 +0000

    Add a selection input parameter - only histograms whose names contain this  input TString will be plotted.

--- AllHistosBrowser.C
+++ AllHistosBrowser.C
@@ -1,6 +1,6 @@
 // A ROOT macro to plot all the open histograms one by one.
 
-int AllHistosBrowser(float delay=1.5, TString opt="")
+int AllHistosBrowser(float delay=1.5, TString opt="", TString select="")
 {
   int noOfPlottedHistos = 0;
   int noOfBrowsables = gDirectory->GetListOfKeys()->GetEntries();
@@ -9,6 +9,7 @@ int AllHistosBrowser(float delay=1.5, TString opt="")
   for (int k=0; k<noOfBrowsables; k++) {
     TObject *myobj = gROOT->FindObject(gDirectory->GetListOfKeys()->At(k)->GetName());
     if ( myobj != 0 && myobj->InheritsFrom("TH1") ) {
+      if ( TString(myobj->GetName()).Index(select)==-1) continue; 
       // if the "same" option is being given, we don't want it to
       // be used for the first histo, otherwise things get ugly
       TString inopt = opt;
