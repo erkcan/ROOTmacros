4792f7348f3406981dd5a790548fab150539490c
erkcan <erkcan>
2006-10-24 16:01:13 +0000

    * Distinguish between whether the random numbers are going to be floating   point or integer using the preprocessor variable _IntRand_. * For integer case, try ChisquareQuantile() function.

--- PoissonToyMC.C
+++ PoissonToyMC.C
@@ -5,7 +5,9 @@
 // It can be thought of a test of how good the random number
 // generation in ROOT is.
 
-float PoissonToyMC(float mean, float CL=0.95, long trials=1e4)
+#define _IntRand_
+
+float PoissonToyMC(double mean, float CL=0.95, long trials=1e4)
 {
   float histrng = TMath::Ceil(mean*2.5);
   TF1 balik("balik","TMath::Poisson(x,[0])",0,histrng);
@@ -13,11 +15,20 @@ float PoissonToyMC(float mean, float CL=0.95, long trials=1e4)
   TH1F *hal = new TH1F("hal","ToyPoisson",histrng,0,histrng);
   long pass=0;
   for (long k=0; k<trials; ++k) {
-    //int uydur = gRandom->Poisson(mean);
-    //int uydur = (int)(TMath::Floor(0.5+(balik.GetRandom())));
+#ifndef _IntRand_
+    // The following works out with a floating point distro:
     float uydur = balik.GetRandom();
-    hal->Fill(uydur);
     if ( TMath::Prob(pow(uydur-mean,2)/mean,1) < CL ) pass++;
+#else
+    int uydur = gRandom->Poisson(mean);
+    //int uydur = (int)(TMath::Floor(0.5+(balik.GetRandom())));
+    // If balik is defined with TMath::PoissonI, use this one:
+    //int uydur = (int)(TMath::Floor(balik.GetRandom()));
+    // But use this one for integers (like numbers of events):
+    if ( 0.5*TMath::ChisquareQuantile(CL,2*uydur+2)>=mean &&
+	 0.5*TMath::ChisquareQuantile(1-CL,2*uydur)<=mean ) pass++;
+#endif
+    hal->Fill(uydur);
   }
   hal->Draw();
   return pass/float(trials);
