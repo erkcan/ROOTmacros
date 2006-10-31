b8141578951b9183dc7a082255015a9277fea6e5
erkcan <erkcan>
2006-10-31 13:41:55 +0000

    Add DrawLine.C.

--- util.C
+++ util.C
@@ -1,5 +1,6 @@
 {
 gROOT->LoadMacro("AllHistosBrowser.C");
+gROOT->LoadMacro("DrawLine.C");
 gROOT->LoadMacro("ErrorPropagation.C");
 gROOT->LoadMacro("PlotASCII.C");
 gROOT->LoadMacro("SameHistoPlotter.C");
