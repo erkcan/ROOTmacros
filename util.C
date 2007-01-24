97910f714a27d7c93ad40f23b001388338178640
erkcan <erkcan>
2007-01-24 21:51:16 +0000

    OutputToFile() added.

--- util.C
+++ util.C
@@ -2,6 +2,7 @@
 gROOT->LoadMacro("AllHistosBrowser.C");
 gROOT->LoadMacro("DrawLine.C");
 gROOT->LoadMacro("ErrorPropagation.C");
+gROOT->LoadMacro("OutputToFile.C");
 gROOT->LoadMacro("PlotASCII.C");
 gROOT->LoadMacro("SameHistoPlotter.C");
 }
