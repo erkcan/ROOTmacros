09d98de5a1355f2cdc5884d599a4dac90c9d01dc
erkcan <erkcan>
2007-10-08 16:01:17 +0000

    Preprocessor option for Gaussian, more internal doc, remove existing histo  while 'new'ing to stop ROOT from complaining.

--- PoissonToyMC.C
+++ PoissonToyMC.C
@@ -5,14 +5,23 @@
 // It can be thought of a test of how good the random number
 // generation in ROOT is.
 
-#define _IntRand_
+#define _IntRand_  // if defined, generate integers
+#define _Poisson_  // if not defined, use Gaussian
 
 float PoissonToyMC(double mean, float CL=0.95, long trials=1e4)
 {
   float histrng = TMath::Ceil(mean*2.5);
-  TF1 balik("balik","TMath::Poisson(x,[0])",0,histrng);
+#ifdef _Poisson_
+  TF1 balik("balik","TMath::PoissonI(x,[0])",0,histrng);
+  // TMath::PoissonI(x,[0]) = TMath::Power([0],TMath::Floor(x))
+  //                /exp([0])/TMath::Factorial(TMath::Floor(x))
+#else
+  TF1 balik("balik","TMath::Gaus(x,[0],[1])",0,histrng);
+  balik.SetParameter(1,sqrt(mean));
+#endif
   balik.SetParameter(0,mean);
-  TH1F *hal = new TH1F("hal","ToyPoisson",histrng,0,histrng);
+  TH1F *hal; if (hal=(TH1F*)gROOT->FindObject("hal")) delete hal;
+  hal = new TH1F("hal","ToyPoisson",histrng,0,histrng);
   long pass=0;
   for (long k=0; k<trials; ++k) {
 #ifndef _IntRand_
@@ -20,13 +29,14 @@ float PoissonToyMC(double mean, float CL=0.95, long trials=1e4)
     float uydur = balik.GetRandom();
     if ( TMath::Prob(pow(uydur-mean,2)/mean,1) < CL ) pass++;
 #else
-    int uydur = gRandom->Poisson(mean);
+    // Use this one for integers (like numbers of events):
+    //int uydur = gRandom->Poisson(mean);
     //int uydur = (int)(TMath::Floor(0.5+(balik.GetRandom())));
     // If balik is defined with TMath::PoissonI, use this one:
-    //int uydur = (int)(TMath::Floor(balik.GetRandom()));
-    // But use this one for integers (like numbers of events):
+    int uydur = (int)(TMath::Floor(balik.GetRandom()));
     if ( 0.5*TMath::ChisquareQuantile(CL,2*uydur+2)>=mean &&
-	 0.5*TMath::ChisquareQuantile(1-CL,2*uydur)<=mean ) pass++;
+    	 0.5*TMath::ChisquareQuantile(1-CL,2*uydur)<=mean ) pass++;
+    //if ( TMath::Prob(pow(uydur-mean-0.5,2)/mean,1) < CL ) pass++;
 #endif
     hal->Fill(uydur);
   }
