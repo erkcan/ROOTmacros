9609f3cd4b3ae42cdc1923b46dfda9be64fe0841
erkcan <erkcan>
2007-12-21 16:07:43 +0000

    Implement CombineBins properly (might need further testing).

--- ManipulateBins.C
+++ ManipulateBins.C
@@ -1,6 +1,16 @@
-//TH1D inhisto("myhisto","Good histo", 100, 0, 500)
+/* Testing combineBins
+.L ManipulateBins.C
+TH1F myhisto("myhisto","Good histo", 20, 0, 100);
+for (int i=1;i<21;++i) myhisto.SetBinContent(i,32);
+TH1F *newh = CombineBins(myhisto,21,26);
+newh->SetLineColor(4);
+myhisto.Draw()
+newh->Draw("same");
+*/
 
-int combineBins(TH1F inhisto, float xmin, float xmax)
+// A function to make a new histogram by combining the contents of selected
+// consecutive bins in an input histogram
+TH1F* CombineBins(TH1F inhisto, float xmin, float xmax)
 {
   int startbin = inhisto.FindBin(xmin);
   int endbin = inhisto.FindBin(xmax);
@@ -18,21 +28,33 @@ int combineBins(TH1F inhisto, float xmin, float xmax)
     edgeArr[i] = inhisto.GetBinLowEdge(i+1+skipbin);
   }
 
-  TH1F *newhist = new TH1F("newhist","newhist",newNbins,edgeArr);
+  TH1F *newhist = new TH1F("newhist",inhisto.GetTitle(),newNbins,edgeArr);
+  newhist->GetYaxis()->SetTitle(inhisto.GetYaxis()->GetTitle());
+  newhist->GetXaxis()->SetTitle(inhisto.GetXaxis()->GetTitle());
 
+  // Starting from the new underflow bin, up to the new overflow bin,
+  // copy contents for the "untouched" bins. For the new "combination"
+  // bin, copy only the startbin
   for (int i=0; i<=newNbins+1; ++i) {
     int skipbin = 0;
-    if (i>=startbin) skipbin = endbin - startbin;
+    if (i>startbin) skipbin = endbin - startbin;
     newhist->SetBinContent(i, inhisto.GetBinContent(i+skipbin));
     newhist->SetBinError(i, inhisto.GetBinError(i+skipbin));
   }
 
-
+  // Now we have to handle the "combination" bin. We need to add
+  // everything from startbin+1 to endbin. 
+  for (int i=startbin+1; i<=endbin; ++i) {
+    newhist->SetBinContent(startbin, newhist->GetBinContent(startbin)
+			   +inhisto.GetBinContent(i));
+    newhist->SetBinError(startbin, sqrt(pow(newhist->GetBinError(startbin),2)+
+					pow(inhisto.GetBinError(i),2)));
+  }
   /*for (int i=1; i<=newhist->GetNbinsX()+1; ++i)
     cout << newhist->GetBinLowEdge(i) << "\t";
     cout << endl;*/
 
-  return newNbins;
+  return newhist;
 }
 
 
