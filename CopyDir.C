3a315ba1ea45e909409ba203f37f1ebbfdef9e4c
erkcan <erkcan>
2008-05-01 21:36:48 +0000

    Clean development-related comment lines. Properly return no. of bytes written.

--- CopyDir.C
+++ CopyDir.C
@@ -1,21 +1,18 @@
-int CopyDir(TDirectory *sdir, TDirectory *tdir, TString indent="") {
+Long64_t CopyDir(TDirectory *sdir, TDirectory *tdir, TString indent="") {
 
   if ( sdir == 0 || tdir == 0 ) return 0;
   cout << indent << sdir->GetName() << "/" << endl;
   TList *dirlist = sdir->GetListOfKeys();
+  Long64_t nbytes = 0;
   for (int i=0; i<dirlist->GetEntries(); i++) {
     TKey *member = (TKey*)dirlist->At(i);
     if ( member->ReadObj()->InheritsFrom("TDirectory") ) {
-      CopyDir( (TDirectory*)(member->ReadObj()),
-	       tdir->mkdir(member->GetName()), indent+" " ); }
-    else {
-      tdir->cd();
-      member->ReadObj()->Write();
-      sdir->cd(); }
-    //cout << member->GetName() << endl;
+      nbytes += CopyDir( (TDirectory*)(member->ReadObj()),
+			 tdir->mkdir(member->GetName()), indent+" " );
+      continue; }
+    tdir->cd();
+    nbytes += member->ReadObj()->Write();
+    sdir->cd();
   }
-  //sdir->ReadAll();
-  //tdir->cd();
-  //sdir->GetList()->Write();
-  //sdir->cd();
+  return nbytes;
 }
