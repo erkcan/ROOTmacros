40a33a0418f063139c9bc569936160b211dd3041
erkcan <erkcan>
2008-07-09 13:13:17 +0000

    Add HistoAutoLogScale() function.

--- HistoAutoRange.C
+++ HistoAutoRange.C
@@ -54,3 +54,59 @@ void HistoAutoRange() {
   gPad->Modified();
 
 }
+
+
+
+// Loop through all histos in the current gPad and convert to log scale
+//  if it would look better.  The "better" condition is based on comparing
+//  the maximum content for each histo and identifying if the quotient
+//  of any such maxima exceeds a certain factor.  There is also a check
+//  against very "sharp" histograms, where the mean number of entries for
+//  the non-empty bins is much smaller than the max bin content.
+void HistoAutoLogScale(float xfactor=20) {
+
+  if ( gROOT->GetListOfCanvases()->GetEntries() == 0 ) return;
+  TList *glist = gPad->GetListOfPrimitives();
+  if ( gPad->GetLogy() ) return;
+
+  float maxhisthi = -1;
+  float maxhistlo = -1;
+  bool sharphisto(false);
+
+  // Now loop through histos and find the extrema
+  for (int i=0; i<glist->GetEntries(); ++i) {
+    TObject *obj = glist->At(i);
+    if ( obj->InheritsFrom("TH1") ) {
+
+      // If this is a 2D histogram, sorry, nothing to do yet.
+      if ( obj->InheritsFrom("TH2") ) return;
+
+      TH1 *histo = (TH1*)obj;
+
+      // If any of the histos have negative content, sorry, nothing to do.
+      if ( histo->GetBinContent(histo->GetMinimumBin()) < 0 ) return;
+
+      float maxhist = histo->GetBinContent(histo->GetMaximumBin());
+
+      if ( maxhisthi < 0 )
+	maxhisthi = maxhistlo = maxhist;
+      else {
+	if ( maxhistlo > maxhist ) maxhistlo = maxhist;
+	if ( maxhisthi < maxhist ) maxhisthi = maxhist;
+      }
+
+      // If any of the histos have a very sharp peak
+      unsigned int nbinsfull = 0;  float integral = 0;
+      for (int ib=1; ib<=histo->GetNbinsX(); ++ib)
+	if ( histo->GetBinContent(ib) > 0 ) {
+	  nbinsfull++; integral += histo->GetBinContent(ib); }
+      if ( maxhist * nbinsfull > (integral-maxhist) * xfactor ) {
+	sharphisto = true; break; }
+
+    }
+  }
+
+  if ( maxhistlo * xfactor < maxhisthi  ||  sharphisto ) gPad->SetLogy();
+  gPad->Modified();
+
+}
