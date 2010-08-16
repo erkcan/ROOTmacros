7804bb8b558ae452bfb026db161e257dd280abdf
Erkcan Ozcan <erkcan@Erkcans-MacBook.local>
2010-08-16 23:04:18 +0200

    improvements in comments, particularly for OutputToFile.C

--- HistogramOperator.C
+++ HistogramOperator.C
@@ -1,4 +1,4 @@
-// A macro to take 2 histogramd and operate on them bin-by-bin.
+// A macro to take 2 histograms and operate on them bin-by-bin.
 // The cmd string can be anything accepted by ROOT CINT as valid.
 //   It should be a line which gets op1 and op2 and makes a result from them.
 
--- OutputToFile.C
+++ OutputToFile.C
@@ -7,7 +7,14 @@
 // OutputToConsole();
 
 // There are absolutely no guarantees OutputToConsole() will
-// work. Only tested on under Cygwin with ROOT 5.12.
+// work. Only tested on under Cygwin with ROOT 5.12 and
+// MacOSX 10.6.4 with ROOT 5.26.00b.
+
+// This method puts and extra EOF to stdout, which is inconvenient
+// for compiled code. For that try this:
+//  int soi = dup(1);  freopen("/dev/null","w",stdout);
+//  ...
+//  char buf[20];  sprintf(buf, "/dev/fd/%d", soi);  freopen(buf, "w", stdout);
 
 void OutputToFile(char* filename, bool append=1) {
   if (append) { freopen(filename,"a",stdout); return; }
