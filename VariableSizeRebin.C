541fba3607c21e46b0e6d64cf653cd41853dafe3
Erkcan Ozcan <erkcan@Erkcans-MacBook.local>
2010-11-03 04:37:52 +0100

    VariableSizeRebin.C: Now can handle TH2x.

--- VariableSizeRebin.C
+++ VariableSizeRebin.C
@@ -1,30 +1,40 @@
-// A function to convert a histogram into a variable-size bin histogram of
-//  desired binning.
+// A function to convert a 1D or 2D histogram into a variable-size bin
+//  histogram of desired binning.
 
 // Arguments are similar to the TH1 constructor with variable-size bins.
-//     nbins  : number of bins
-//     xbins  : array of low-edges for each bin
-//              This is an array of size nbins+1
+//     nbinsx  : number of bins
+//     xbins   : array of low-edges for each bin
+//               This is an array of size nbinsx+1
+//     axisname: the axis to be rebinned, it should be "x" or "y"
 
 // While initially the code was written to do everything manually, now it
-//   internally calls the TH1::Rebin() method. Still it is a useful wrapper,
-//   since it can handle cases with binedges not aligning properly.
+//   internally calls the TH1::Rebin() method for 1D histograms. Still it
+//   is a useful wrapper even for 1D histograms, since it can handle cases
+//   with binedges not aligning properly.
 
-TH1D* VariableSizeRebin(TH1* inhisto, unsigned int nbinsx, double *xbins)
+// First implementation: Nov 1-3, 2010, veo.
+
+TH1* VariableSizeRebin(TH1* inhisto, unsigned int nbinsx,
+		       double *xbins, TString axisname="x")
 {
   if ( nbinsx == 0 ) {
     cout << "Error! nbinsx must be non-zero." << endl; return 0; }
 
+  if ( axisname == "y"  &&  !inhisto->InheritsFrom("TH2") ) {
+    cout << "No y-axis defined for " << inhisto->GetName() << endl; return 0; }
+
   double *edgeArr = new double[nbinsx+2]; // an extra bin for safety
+
+  TAxis *axis = (axisname=="y" ? inhisto->GetYaxis() : inhisto->GetXaxis());
 
   unsigned int nbins = 0; // number of bins for the new histogram
   unsigned int j = 0; // dummy bin index (to be used as a pointer)
 
-  for ( unsigned int i=0; i<=inhisto->GetNbinsX()+1; ++i ) {
+  for ( unsigned int i=0; i<=axis->GetNbins()+1; ++i ) {
 
     if ( j > nbinsx ) break;
 
-    double ble = inhisto->GetBinLowEdge(i);
+    double ble = axis->GetBinLowEdge(i);
 
     if ( xbins[j] > ble ) continue;
 
@@ -43,7 +53,7 @@ TH1D* VariableSizeRebin(TH1* inhisto, unsigned int nbinsx, double *xbins)
   // that the input histogram xrange is shorter than what the new binning
   // tried to get. So handle that.
   if ( j <= nbinsx ) {
-    double xmax = inhisto->GetBinLowEdge(inhisto->GetNbinsX()+1);
+    double xmax = axis->GetBinLowEdge(axis->GetNbins()+1);
     if ( xmax>edgeArr[nbins-1] ) {
       edgeArr[nbins]=xmax;
       cout << "Warning! Input histo reached max value of its x-range. "
@@ -59,31 +69,76 @@ TH1D* VariableSizeRebin(TH1* inhisto, unsigned int nbinsx, double *xbins)
     cout << "Warning! nbinsx set to " << nbins
 	 << " instead of " << nbinsx << "." << endl;
 
-  for ( unsigned int i=0; i<=nbins; i++ )
-    cout << "For bin " << i << "\tlowedge= " << edgeArr[i] << endl;
+  //for ( unsigned int i=0; i<=nbins; i++ )
+  //  cout << "For bin " << i+1 << "\tlowedge= " << edgeArr[i] << endl;
 
-  // Now book the new histogram
-  /*
-  TH1D *newhist = new TH1D("newhist",inhisto->GetTitle(),nbins,edgeArr);
-  newhist->GetYaxis()->SetTitle(inhisto->GetYaxis()->GetTitle());
-  newhist->GetXaxis()->SetTitle(inhisto->GetXaxis()->GetTitle());
-  bool sw2 = ( inhisto->GetSumw2N() != 0 );
+  // Now generate the new histogram
+  TH1 *newhist = 0;
 
-  // Fill the new histogram from the input histogram
-  j=0; // reset the dummy bin index
-  for ( unsigned int i=0; i<=inhisto->GetNbinsX()+1; ++i ) {
+  if ( !inhisto->InheritsFrom("TH2") )
+    newhist = inhisto->Rebin(nbins,"newhist",edgeArr);
 
-    double ble = inhisto->GetBinLowEdge(i);
-    if ( edgeArr[j] == ble ) j++; 
-      
-    newhist->SetBinContent( j, newhist->GetBinContent(j)
-			    + inhisto->GetBinContent(i) );
-    if ( sw2 ) newhist->SetBinError(j, sqrt(pow(newhist->GetBinError(j),2) +
-					    pow(inhisto->GetBinError(i),2)));}
+  else {
 
-  newhist->SetEntries(inhisto->GetEntries());
-  */
-  TH1D *newhist = inhisto->Rebin(nbins,"newhist",edgeArr);
+    // Copy the perpendicular axis as it is.
+    TAxis *axisp = (axisname=="y" ? inhisto->GetXaxis() : inhisto->GetYaxis());
+    unsigned int nbinsp = axisp->GetNbins();
+    double *edgeArrp = new double[nbinsp+1];
+    for ( unsigned int i=1; i<=nbinsp+1; ++i )
+      edgeArrp[i] = axisp->GetBinLowEdge(i);
+
+    if ( axisname == "y" ) {
+
+      if ( axisp->IsVariableBinSize() )
+	newhist = new TH2D("newhist", inhisto->GetTitle(),
+			   nbinsp, edgeArrp, nbins, edgeArr);
+      else
+	newhist = new TH2D("newhist", inhisto->GetTitle(),
+			   nbinsp, edgeArrp[0], edgeArrp[nbinsp+1],
+			   nbins, edgeArr);
+
+      if ( axisp->GetLabels() )
+	for ( unsigned int i=1; i<=nbinsp; ++i )
+	newhist->GetXaxis()->SetBinLabel(i, axisp->GetBinLabel(i));
+
+    }
+    else
+      // ToDo: Have not yet implemented the above nice stuff for axisname=="x"
+      newhist = new TH2D("newhist", inhisto->GetTitle(),
+			 nbins, edgeArr, nbinsp, edgeArrp);
+
+    newhist->GetYaxis()->SetTitle(inhisto->GetYaxis()->GetTitle());
+    newhist->GetXaxis()->SetTitle(inhisto->GetXaxis()->GetTitle());
+    bool sw2 = ( inhisto->GetSumw2N() != 0 );
+
+    // Fill the new histogram from the input histogram
+    j=0; // reset the dummy bin index
+    for ( unsigned int i=0; i<=axis->GetNbins()+1; ++i ) {
+
+      double ble = axis->GetBinLowEdge(i);
+      if ( edgeArr[j] == ble ) j++; 
+
+      for ( unsigned int k=0; k<=nbinsp+1; ++k ) {
+
+	int newbin(0), oldbin(0);
+	// Equivalent 1D bin number = binx + (fXaxis.GetNbins()+2)*biny
+	if ( axisname == "y" ) {
+	  newbin = k+j*(nbinsp+2); oldbin = k+i*(nbinsp+2); }
+	else {
+	  newbin = j+k*(nbins+2); oldbin = i+k*(axis->GetNbins()+2); }
+
+	newhist->SetBinContent( newbin, newhist->GetBinContent(newbin)
+				+ inhisto->GetBinContent(oldbin) );
+	if ( sw2 )
+	  newhist->SetBinError( newbin,
+			        sqrt(pow(newhist->GetBinError(newbin),2) +
+				     pow(inhisto->GetBinError(oldbin),2)) );
+      }
+    }
+
+    newhist->SetEntries(inhisto->GetEntries());
+
+  }
 
   //newhist->Draw();
   delete [] edgeArr;
